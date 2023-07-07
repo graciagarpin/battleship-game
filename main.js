b8667f3bfc09a3ee3c30b095cdcd2e718e1be85a
LAPTOP-BQNGAEFP\graci <graciagarpin>
2023-07-07 11:11:55 +0200

    add isSunk method to determine if a ship is sunk

--- main.js
+++ main.js
@@ -28,10 +28,23 @@ var model = {
       var index = ship.locations.indexOf(guess);
       if (index >= 0) {
         ship.hits[index] = 'hit';
+        if (this.isSunk(ship)) {
+          view.displayMessage('You sank my battleship');
+          this.shipsSunk++;
+        }
         return true;
       }
     }
     return false;
+  },
+
+  isSunk: function (ship) {
+    for (var i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== 'hit') {
+        return false;
+      }
+    }
+    return true;
   },
 };
 
