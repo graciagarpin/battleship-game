ad63574e6adc17f4b2e3cff26584c8c47165bf6f
LAPTOP-BQNGAEFP\graci <graciagarpin>
2023-07-07 11:16:46 +0200

    add test (console) to fire method

--- main.js
+++ main.js
@@ -29,7 +29,6 @@ var model = {
       if (index >= 0) {
         ship.hits[index] = 'hit';
         if (this.isSunk(ship)) {
-          view.displayMessage('You sank my battleship');
           this.shipsSunk++;
         }
         return true;
@@ -49,9 +48,20 @@ var model = {
 };
 
 //View test:
-view.displayMiss('00');
-view.displayHit('34');
-view.displayMiss('55');
-view.displayHit('12');
-view.displayMiss('25');
-view.displayHit('26');
+// view.displayMiss('00');
+// view.displayHit('34');
+// view.displayMiss('55');
+// view.displayHit('12');
+// view.displayMiss('25');
+// view.displayHit('26');
+
+// Fire test:
+model.fire('53');
+model.fire('06');
+model.fire('16');
+model.fire('26');
+model.fire('34');
+model.fire('24');
+model.fire('44');
+model.fire('12');
+model.fire('11');
